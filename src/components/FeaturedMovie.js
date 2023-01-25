55bd62cf54f4d35796a26bd72a4b977548225bc1
Carsten Lund <clu@rts.dk>
2023-01-25 09:32:49 +0100

    naviagationlink

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -7,20 +7,17 @@ const FeaturedMovie = () => {
   useEffect(() => {
     axios(
       "https://api.themoviedb.org/3/trending/movie/day?language=en&api_key=c28b09251184479f999a2baafd615444"
-    ).then((response) => setFeatured(response.data.results[1]));
+    ).then((response) => setFeatured(response.data.results[5]));
   }, []);
   return (
     featured && (
-      <section
-        className="relative"
-        style={{ width: "56rem", aspectRatio: 2.2 }}
-      >
-        <div className="absolute left-0 bottom-0 z-10 h-1/2 w-full rounded-b-3xl bg-gradient-to-t from-gray-600 to-transparent"></div>
+      <section className="relative aspect-[2.2] w-[56rem] overflow-hidden rounded-3xl">
+        <div className="absolute left-0 bottom-0 z-10 h-1/2 w-full bg-gradient-to-t from-gray-800 to-transparent mix-blend-multiply"></div>
         <div className="absolute bottom-8 left-8 z-20 w-[32rem]">
           <p className="m-0 font-bold text-white">{featured.overview}</p>
         </div>
         <img
-          className="h-full w-full rounded-3xl object-cover"
+          className="h-full w-full object-cover"
           src={`https://image.tmdb.org/t/p/original${featured.backdrop_path}`}
           alt=""
         />
--- src/components/NavigationLink.js
+++ src/components/NavigationLink.js
@@ -3,19 +3,26 @@ import { css } from "@emotion/react";
 
 import { NavLink } from "react-router-dom";
 
-const NavigationLink = ({ children }) => {
+const NavigationLink = ({ children, to }) => {
   const styles = css`
     padding: 2rem;
     color: gray;
 
     &.active {
+      color: teal;
+      background-color: #0080806a;
     }
 
     & svg {
       transform: translateY(0.125rem);
+      margin-right: 0.75rem;
     }
   `;
-  return <NavLink css={styles}>{children}</NavLink>;
+  return (
+    <NavLink css={styles} to={to}>
+      {children}
+    </NavLink>
+  );
 };
 
 export default NavigationLink;
